bba35a35466293a2fa71e2deb93a7fc56087e239
Eden.Han <32676195+edenhanjing@users.noreply.github.com>
2019-07-24 16:30:19 +0800

    Update settings.py

--- company/company/settings.py
+++ company/company/settings.py
@@ -84,24 +84,24 @@ WSGI_APPLICATION = 'company.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'company',
-        'USER': 'root',
-        'PASSWORD': 'xxxxxxxx',
-        'PORT': '3306',
-        'HOST':'127.0.0.1',
-        'charset':'utf8',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'company',
+#         'USER': 'root',
+#         'PASSWORD': 'xxxxxxxx',
+#         'PORT': '3306',
+#         'HOST':'127.0.0.1',
+#         'charset':'utf8',
+#     }
+# }
 
 
 # Password validation
